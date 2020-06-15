0f01db999a4e005a4b21c183b094de5d07881360
knunu <burette@hanyang.ac.kr>
2020-06-15 12:57:44 +0900

    [WIP] initiate leetcode project.

---
+++ leetcode/src/CheapestFlightsWithinKStops.kt
@@ -0,0 +1,10 @@
+class CheapestFlightsWithinKStops {
+    fun findCheapestPrice(n: Int, flights: Array<IntArray>, src: Int, dst: Int, K: Int): Int {
+        return 0
+    }
+}
+
+fun main() {
+    val cheapestPrice: Int = CheapestFlightsWithinKStops().findCheapestPrice()
+    println(cheapestPrice)
+}
